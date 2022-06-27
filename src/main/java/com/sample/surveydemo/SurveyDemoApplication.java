9193b1d6719316009bdb6307a790ea2ed5930fc5
vendra00 <gabriel_vendramini@hotmail.com>
2022-06-27 20:48:10 +0200

    1 commit

---
+++ src/main/java/com/sample/surveydemo/SurveyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sample.surveydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveyDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sample/surveydemo/SurveyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.surveydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
