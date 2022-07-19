35708eee746dddfe5f65fee5554899c182ebde7a
vendra00 <gabriel_vendramini@hotmail.com>
2022-07-19 18:45:31 +0200

    some refactor on the mappers

--- src/main/java/com/sample/surveydemo/controller/OfferController.java
+++ src/main/java/com/sample/surveydemo/controller/OfferController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
-
 @Slf4j
 @Api
 @RestController
